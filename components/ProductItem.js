aa49dab8469e3e9f0da4fe410836ea1337ef2cb0
ibrahimkeyboad <ibrahimmwanga07@gamil.com>
2023-06-30 16:36:59 +0300

    push to git hub

---
+++ components/ProductItem.js
@@ -0,0 +1,31 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link';
+import React from 'react';
+
+export default function ProductItem({ product, addToCartHandler }) {
+  return (
+    <div className="card">
+      <Link href={`/product/${product.slug}`}>
+        <img
+          src={product.image}
+          alt={product.name}
+          className="rounded shadow object-cover h-64 w-full"
+        />
+      </Link>
+      <div className="flex flex-col items-center justify-center p-5">
+        <Link href={`/product/${product.slug}`}>
+          <h2 className="text-lg">{product.name}</h2>
+        </Link>
+        <p className="mb-2">{product.brand}</p>
+        <p>${product.price}</p>
+        <button
+          className="primary-button"
+          type="button"
+          onClick={() => addToCartHandler(product)}
+        >
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+}
